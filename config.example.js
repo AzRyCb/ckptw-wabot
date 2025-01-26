efc3294381e4d1160b4d40e8d0d1bb3a5f4bdd72
AzRyCb <azry695@gmail.com>
2025-01-27 00:22:24 +0700

    update config.example.js

--- config.example.js
+++ config.example.js
@@ -10,12 +10,12 @@ const {
 global.config = {
     // Informasi bot dasar
     bot: {
-        name: "UmbrX", // Nama bot
+        name: "NeuBot", // Nama bot
         prefix: /^[°•π÷×¶∆£¢€¥®™+✓_=|/~!?@#%^&.©^]/i, // Karakter awalan perintah yang diizinkan
         phoneNumber: "", // Nomor telepon bot (opsional jika menggunakan QR code)
-        thumbnail: "https://itsreimau.is-a.dev/umbrx/dist/img/thumbnail/umbrx.jpg", // Gambar thumbnail bot
-        website: "https://chat.whatsapp.com/FxEYZl2UyzAEI2yhaH34Ye", // Website untuk WhatsApp bot
-        groupJid: "" // JID untuk group bot (opsional jika tidak menggunakan requireBotGroupMembership)
+        thumbnail: "https://img.freepik.com/premium-vector/robot-icon-chat-bot-sign-support-service-concept-chatbot-character-flat-style_41737-796.jpg?w=1060", // Gambar thumbnail bot
+        website: "https://chat.whatsapp.com/GQ6RHr5jPy920dPVTpq4uC", // Website untuk WhatsApp bot
+        groupJid: "120363391379200206@g.us" // JID untuk group bot (opsional jika tidak menggunakan requireBotGroupMembership)
     },
 
     // Pesan bot yang disesuaikan untuk situasi tertentu
@@ -44,10 +44,10 @@ global.config = {
 
     // Informasi owner bot
     owner: {
-        name: "", // Nama owner bot
-        organization: "", // Nama organisasi owner bot
-        id: "", // Nomor telepon owner bot
-        co: [""] // Nomor co-owner bot
+        name: "z_ryyw", // Nama owner bot
+        organization: "Npm Community", // Nama organisasi owner bot
+        id: "6283879175089", // Nomor telepon owner bot
+        co: ["6285211425532"] // Nomor co-owner bot
     },
 
     // Kunci API
@@ -59,25 +59,25 @@ global.config = {
 
     // Stiker bot
     sticker: {
-        packname: "", // Nama paket stiker
-        author: "github.com/itsreimau/ckptw-wabot" // Pembuat stiker
+        packname: "Created By NeuBot", // Nama paket stiker
+        author: "https://instagram.com/z_ryyw" // Pembuat stiker
     },
 
     // Sistem bot
     system: {
-        alwaysOnline: true, // Bot selalu berstatus "online"
+        alwaysOnline: falss, // Bot selalu berstatus "online"
         autoMention: false, // Bot otomatis mention seseorang dalam pesan yang dikirim
-        autoRead: true, // Bot baca pesan otomatis
+        autoRead: false, // Bot baca pesan otomatis
         autoTypingOnCmd: true, // Tampilkan status "sedang mengetik" saat memproses perintah
         cooldown: 10 * 1000, // Jeda antar perintah (ms)
         port: 3000, // Port (jika pakai server)
         restrict: false, // Batasi akses perintah
-        requireBotGroupMembership: false, // Harus gabung grup bot
+        requireBotGroupMembership: true, // Harus gabung grup bot
         selfOwner: false, // Bot jadi owner sendiri
         selfReply: true, // Bot balas pesan bot sendiri
         timeZone: "Asia/Jakarta", // Zona waktu bot
         useCoin: true, // Pakai koin
-        usePairingCode: false, // Pakai kode pairing untuk koneksi
+        usePairingCode: true, // Pakai kode pairing untuk koneksi
         useServer: false // Jalankan bot dengan server
     }
 };
